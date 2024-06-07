c0548f36ce65897218e828f3ee2f2ff4760ad008
jhonestiago <jonestiago.94@gmail.com>
2024-06-07 19:36:29 -0300

    ContaCorrente

---
+++ banco-digital/src/model/ContaCorrente.java
@@ -0,0 +1,18 @@
+package model;
+
+public class ContaCorrente extends ContaBancaria {
+
+    // Construtor
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        System.out.println("Titular: " + this.cliente.getNome());
+        System.out.println("Agência: " + this.agencia);
+        System.out.println("Número: " + this.numero);
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
+    }
+}
