0bb112cfc77fb2a27c8a7c1628a344bdef541707
jhonestiago <jonestiago.94@gmail.com>
2024-06-07 19:39:21 -0300

    ContaCorrente e ContaPoupanca

--- banco-digital/src/model/ContaPoupanca.java
+++ banco-digital/src/model/ContaPoupanca.java
@@ -1,19 +1,19 @@
 package model;
 
-public class ContaCorrente extends ContaBancaria {
+public class ContaPoupanca extends ContaBancaria {
 
     // Construtor
-    public ContaCorrente(Cliente cliente) {
+    public ContaPoupanca(Cliente cliente) {
         super(cliente);
     }
 
     // Métodos
     @Override
     public void imprimirExtrato() {
-        System.out.println("=== Extrato Conta Corrente ===");
         System.out.println("Titular: " + this.cliente.getNome());
         System.out.println("Agência: " + this.agencia);
         System.out.println("Número: " + this.numero);
         System.out.println(String.format("Saldo: %.2f", this.saldo));
     }
+
 }
