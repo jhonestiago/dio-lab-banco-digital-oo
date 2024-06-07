a897549fb62f7e5d88faebbf064dacb91b8869dc
jhonestiago <jonestiago.94@gmail.com>
2024-06-07 19:50:16 -0300

    BancoService

--- banco-digital/src/service/BancoService.java
+++ banco-digital/src/service/BancoService.java
@@ -1,5 +1,6 @@
 package service;
 
+import model.Cliente;
 import model.ContaBancaria;
 
 public class BancoService {
@@ -10,13 +11,19 @@ public class BancoService {
     }
 
     // Realizar Saque
-    public boolean realizarSaque(ContaBancaria conta, double valor) {
-        return conta.sacar(valor);
+    public boolean realizarSaque(ContaBancaria conta, double valor, String senha) {
+        if (conta.getCliente().validarSenha(senha)) {
+            return conta.sacar(valor);
+        }
+        return false;
     }
 
     // Realizar Transferencia
-    public boolean realizarTransferencia(ContaBancaria contaOrigem, ContaBancaria contaDestino, double valor) {
-        return contaOrigem.transferir(valor, contaDestino);
+    public boolean realizarTransferencia(ContaBancaria contaOrigem, ContaBancaria contaDestino, double valor, String senha) {
+        if (contaOrigem.getCliente().validarSenha(senha)) {
+            return contaOrigem.transferir(valor, contaDestino);
+        }
+        return false;
     }
 
 }
